232af78005a39d8dbd140f0c882e1239d04e2f61
AD\BYA007HSS <k-kurashita@za.biglobe.co.jp>
2024-04-19 10:11:02 +0900

    no message

---
+++ test/GearSwap/data/export/髱帝ｭ疲ｮｴ繧願｣・ｙ.lua
@@ -0,0 +1,22 @@
+sets.exported={
+    main="ネイグリング",
+    sub={ name="タンモガイ+1", augments={'Path: A',}},
+    ammo="銀銭",
+    head="マリグナスシャポー",
+    body="マリグナスタバード",
+    hands="ＨＳバズバンド+2",
+    legs="ＨＳタイト+2",
+    feet="マリグナスブーツ",
+    neck="サンクトネックレス",
+    waist="エスカンストーン",
+    left_ear="ネリチックピアス",
+    right_ear={ name="ハシシンピアス", augments={'System: 1 ID: 1676 Val: 0','Accuracy+6','Mag. Acc.+6',}},
+    left_ring="ペトロフリング",
+    right_ring="シーリチリング+1",
+    back={ name="コンフラワーケープ", augments={'MP+20','Accuracy+3','Blue Magic skill +10',}},
+}
+
+
+
+    main="ネイグリング",
+    sub={ name="タンモガイ+1", augments={'Path: A',}},
---
+++ test/GearSwap/data/export/鮟帝ｭ熱B.lua
@@ -0,0 +1,17 @@
+sets.exported={
+    main={ name="グリオアヴァール", augments={'Magic burst dmg.+3%','MND+8','Mag. Acc.+23','"Mag.Atk.Bns."+21',}},
+    sub="ニオビッドストラップ",
+    ammo={ name="ガストリタスラム+1", augments={'Path: A',}},
+    head={ name="マーリンフード", augments={'Mag. Acc.+16','Magic burst dmg.+6%','INT+10','"Mag.Atk.Bns."+7',}},
+    body="ＷＣコート+2",
+    hands="ＳＰグローブ+3",
+    legs="ＷＣショウス+2",
+    feet="ＳＰサボ+3",
+    neck="水影の首飾り",
+    waist="エスカンストーン",
+    left_ear="マリグナスピアス",
+    right_ear={ name="ウィッケピアス", augments={'System: 1 ID: 1676 Val: 0','Mag. Acc.+7',}},
+    left_ring="ジャリリング",
+    right_ring="ローカスリング",
+    back={ name="タラナスケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10',}},
+}
